d18cdfb65c56595759d74f7fe6b157a17e6366e1
Dmitrii Akulin <dimaakulin@yandex.ru>
2023-07-07 12:04:53 +0300

    Initial commit

---
+++ AdressFirstD.A.cpp
@@ -0,0 +1,75 @@
+﻿// AdressFirstD.A
+
+#include <Windows.h>
+#include <iostream>
+#include <fstream>
+
+using std::cin;
+using std::cout;
+using std::endl;
+using std::string;
+
+class Adress {
+private:
+    string city_name_ ;
+    string street_name_ ;
+    int num_house_;
+    int num_flat_ ;
+
+public:
+    
+     //конструктор без параметров
+      Adress():city_name_("unknown"), street_name_("unknown"), num_house_(0), num_flat_(0) {}
+
+     //конструктор с параметрами
+      Adress(string city_name_, string street_name_, int num_house_, int num_flat_):
+          city_name_(city_name_), street_name_(street_name_), num_house_(num_house_), num_flat_(num_flat_)
+          {}
+        
+     //вывод в файл  
+      void Get_Output_Address(Adress *arr, int first) {
+            std::ofstream  out("out.txt");            
+            out << first << endl;
+            for (int i = 0; i < first; i++) {
+               
+               out << arr[i].city_name_ << ", " << arr[i].street_name_ << ", " << arr[i].num_house_ << ", "
+               << arr[i].num_flat_ << endl;
+            }
+
+            out.close();
+      }
+
+      
+     //доступ и запись в массив.
+      void AcsessSetData() { 
+                 
+                 std::ifstream in("in.txt");
+                 if (!in.is_open()) std::exit(0);
+                 int first; in >> first;
+                 
+                 Adress* adress_arr = new Adress[first];//создаем массив
+                 
+                 for (int i = 0; i < first; i++) {  //читаем из файла в массив
+                          in >> adress_arr[i].city_name_;
+                          in >> adress_arr[i].street_name_;
+                          in >> adress_arr[i].num_house_;
+                          in >> adress_arr[i].num_flat_;
+                 }
+
+                 Get_Output_Address(adress_arr, first);
+
+                 delete [] adress_arr;
+                 in.close();
+               
+               
+          
+       }
+};
+int main()
+{     
+     
+      Adress adress("unknown", "unknown", 0, 0);      
+      adress.AcsessSetData();
+      
+    return 0;
+}
